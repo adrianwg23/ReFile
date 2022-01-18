dbf1c2fdf50c17440938402f3fe007b34b705ae8
adrianwg23 <adrianwg23@hotmail.com>
2022-01-17 20:38:45 -0500

    init commit

---
+++ src/test/java/com/example/ReFile/ReFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
