1b099731936647ee0618f97910d9c812af149bde
adrianwg23 <adrianwg23@hotmail.com>
2022-01-31 10:33:33 -0500

    implement refresh listener

--- src/main/java/com/example/refile/repository/credentials/InMemoryCredentialStore.java
+++ src/main/java/com/example/refile/repository/credentials/InMemoryCredentialStore.java
@@ -1,13 +1,17 @@
 package com.example.refile.repository.credentials;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.CredentialRefreshListener;
+import com.google.api.client.auth.oauth2.TokenErrorResponse;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class InMemoryCredentialStore implements CredentialStore {
+public class InMemoryCredentialStore implements CredentialStore, CredentialRefreshListener {
 
     private final Map<Long, Credential> credentialMap = new HashMap<>();
 
@@ -19,5 +23,15 @@ public class InMemoryCredentialStore implements CredentialStore {
     @Override
     public Credential getCredential(Long userId) {
         return credentialMap.get(userId);
+    }
+
+    @Override
+    public void onTokenResponse(Credential credential, TokenResponse tokenResponse) throws IOException {
+        System.out.println("refreshed: " + credential);
+    }
+
+    @Override
+    public void onTokenErrorResponse(Credential credential, TokenErrorResponse tokenErrorResponse) throws IOException {
+
     }
 }
