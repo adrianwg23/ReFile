d3f7b8f14242c7ce0abb296059c8d9207b9f7ca9
adrianwg23 <adrianwg23@hotmail.com>
2022-01-31 00:09:41 -0500

    add required columns for attachments table

--- src/main/java/com/example/refile/model/Attachment.java
+++ src/main/java/com/example/refile/model/Attachment.java
@@ -1,9 +1,12 @@
 package com.example.refile.model;
 
+import com.example.refile.util.StringListConverter;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @Entity
@@ -13,8 +16,24 @@ public class Attachment {
 
     @Id
     @GeneratedValue
+    @Column(name = "attachment_id")
     private Long attachmentId;
+
     @ManyToOne
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "user_id")
     private User user;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", updatable = false)
+    private Date createdDate;
+
+    @Convert(converter = StringListConverter.class)
+    @Column(name = "label_ids")
+    private List<String> labelIds;
+
+    private String url;
+    private String extension;
+    private String sender;
+    private String thread;
+    private String subject;
 }
---
+++ src/main/java/com/example/refile/util/StringListConverter.java
@@ -0,0 +1,4 @@
+package com.example.refile.util;
+
+public class StringListConverter {
+}
