8f037e0b9f26c956bcf6f16a63dae283a7a61ee1
adrianwg23 <adrianwg23@hotmail.com>
2022-02-11 17:36:00 -0500

    read client secrets from application.yml

--- src/main/java/com/example/refile/service/CredentialService.java
+++ src/main/java/com/example/refile/service/CredentialService.java
@@ -4,11 +4,11 @@ import com.example.refile.repository.credentials.CredentialStore;
 import com.example.refile.repository.credentials.InMemoryCredentialStore;
 import com.example.refile.util.HttpUtil;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.json.gson.GsonFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -18,15 +18,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CredentialService {
 
-    private final GoogleClientSecrets googleClientSecrets;
     private final CredentialStore credentialStore;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
 
     @SneakyThrows
     public void saveCredential(Long userId, String accessToken, String refreshToken) {
         Credential credential = new GoogleCredential.Builder()
                 .setJsonFactory(GsonFactory.getDefaultInstance())
                 .setTransport(HttpUtil.getHttpTransport())
-                .setClientSecrets(googleClientSecrets)
+                .setClientSecrets(clientId, clientSecret)
                 .setRefreshListeners(Collections.singleton((InMemoryCredentialStore) credentialStore))
                 .build()
                 .setAccessToken(accessToken)
