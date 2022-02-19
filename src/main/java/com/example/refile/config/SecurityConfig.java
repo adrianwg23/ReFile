cb56ab6a02cddfcf036acb6c5aca3804e646e848
adrianwg23 <adrianwg23@hotmail.com>
2022-02-19 02:39:52 -0500

    allow x-xsrf-token header

--- src/main/java/com/example/refile/config/SecurityConfig.java
+++ src/main/java/com/example/refile/config/SecurityConfig.java
@@ -70,7 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type",
+                "X-Requested-With", "X-XSRF-TOKEN"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
