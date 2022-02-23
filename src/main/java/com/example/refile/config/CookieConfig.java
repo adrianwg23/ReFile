99a749831fb67fd8c6acec7a7d293589bddd8b1f
adrianwg23 <adrianwg23@hotmail.com>
2022-02-23 00:16:15 -0500

    use legacy cookie processor

--- src/main/java/com/example/refile/config/CookieConfig.java
+++ src/main/java/com/example/refile/config/CookieConfig.java
@@ -1,16 +1,25 @@
 package com.example.refile.config;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-public class SameSiteCookieConfig {
+public class CookieConfig {
 
     @Bean
     @ConditionalOnProperty(name="spring.profiles.active", havingValue="test")
     public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
         return CookieSameSiteSupplier.ofNone().whenHasNameMatching("XSRF-TOKEN");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (factory) -> factory.addContextCustomizers(
+                (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
     }
 }
