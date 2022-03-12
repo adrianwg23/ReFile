1fc0673b8df7fabc92d7f6bfbf40154997b80d84
adrianwg23 <adrianwg23@hotmail.com>
2022-03-12 17:41:10 -0500

    test

--- src/main/java/com/example/refile/model/Attachment.java
+++ src/main/java/com/example/refile/model/Attachment.java
@@ -34,40 +34,30 @@ public class Attachment {
     private Date createdDate;
 
     @Convert(converter = StringSetConverter.class)
-    @Column(name = "label_ids", length = 300)
     private Set<String> labelIds = new HashSet<>();
 
     @Convert(converter = StringSetConverter.class)
-    @Column(name = "categories", length = 301)
     private Set<String> categories = new HashSet<>();
 
     @Convert(converter = StringSetConverter.class)
-    @Column(name = "cc", length = 302)
     private Set<String> cc = new HashSet<>();
 
     @Convert(converter = StringSetConverter.class)
-    @Column(name = "receiver", length=256)
+    @Column(name = "receiver", length=1000)
     private Set<String> receiver = new HashSet<>();
 
     @JsonIgnore
     @Column(name = "g_id", length = 1000)
     private String gId;
 
-    @Column(name = "name", length=257)
     private String name;
-    @Column(name = "extension", length=258)
     private String extension;
-    @Column(name = "sender", length=259)
     private String sender;
-    @Column(name = "senderEmail", length=260)
     private String senderEmail;
-    @Column(name = "thread", length=261)
     private String thread;
-    @Column(name = "subject", length=262)
     private String subject;
-    @Column(name = "importance", length=263)
     private String importance;
-    @Column(name = "body", length = 65535)
+    @Column(name = "body", length = 1000000)
     private String body;
 
     @Column(length = 500)
